b6173902be6d739a68ebea0b569b6b19dfcfed00
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-11-03 22:15:56 +0800

    set an event listener to save bookmark into array in localstorage and get them when windows load

--- Bookmark Manager/bookmark.js
+++ Bookmark Manager/bookmark.js
@@ -1,4 +1,22 @@
 let urlInput = document.getElementById('urlName')
 let bookmarkBtn = document.getElementById('bookmark')
 let bookmarkDisplay = document.getElementById('bookmarkDisplay')
+let url = []
 
+bookmarkBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    const input = urlInput.value
+    url.push(input)
+
+    const stringifyUrl = JSON.stringify(url)
+    localStorage.setItem('url', stringifyUrl)
+
+})
+
+window.onload = function (){
+    const parsed = JSON.parse(localStorage.getItem('url'))
+    if(parsed){
+        url = parsed
+    }
+}
