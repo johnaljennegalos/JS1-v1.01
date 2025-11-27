c407bab1311ae7100cbbf46e24b2d26cfe03cc26
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-11-27 22:18:33 +0800

    get their respective ID's

---
+++ Text To Speech/speech.js
@@ -0,0 +1,5 @@
+let userInput = document.getElementById('user-text');
+let speedSlider = document.getElementById('speed-slider');
+let pitchSlider = document.getElementById('pitch-slider');
+let speakBtn = document.getElementById('speak-button');
+let resetBtn = document.getElementById('reset-button');
