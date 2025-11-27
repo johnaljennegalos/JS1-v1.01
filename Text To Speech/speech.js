554d1a0f4482e73e020068bdc8e3a67d8f736725
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-11-27 23:25:05 +0800

    created an event listener for speedSlider/pitch and event listener for speak button. Also adjust the range value

--- Text To Speech/speech.js
+++ Text To Speech/speech.js
@@ -1,5 +1,28 @@
 let userInput = document.getElementById('user-text');
 let speedSlider = document.getElementById('speed-slider');
+let speedValue = document.getElementById('speed-value');
 let pitchSlider = document.getElementById('pitch-slider');
+let pitchValue = document.getElementById('pitch-value');
 let speakBtn = document.getElementById('speak-button');
-let resetBtn = document.getElementById('reset-button');
+let resetBtn = document.getElementById('reset-button')
+
+speedSlider.addEventListener('input', () => {
+    speedValue.textContent = speedSlider.value
+})
+
+pitchSlider.addEventListener('input', () => {
+    pitchValue.textContent = pitchValue.value
+})
+
+speakBtn.addEventListener('click', () => {
+
+    const input = userInput.value;
+
+    const utterance = new SpeechSynthesisUtterance(input)
+
+    utterance.rate = speedSlider.value
+
+    utterance.pitch = pitchSlider.value
+
+    window.speechSynthesis.speak(utterance)
+})
