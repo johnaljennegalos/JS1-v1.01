985389354415bce6ead7f86c004f3a1a05e4f051
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-12-11 22:04:10 +0800

    add an event listener to hide and display respective container

---
+++ BMI Calculator/bmi.js
@@ -0,0 +1,15 @@
+let formEl =  document.getElementById('form');
+let standardContainer = document.getElementById('standard-container');
+let standardBtn1 = document.getElementById('standard1-btn')
+let metricBtn1 = document.getElementById('metric1-btn')
+
+let metricContainer = document.getElementById('metric-container');
+let standardBtn2 = document.getElementById('standard2-btn')
+let metricBtn2 = document.getElementById('metric2-btn')
+
+standardBtn2.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    standardContainer.classList.remove('hide')
+    metricContainer.classList.add('hide')
+})
