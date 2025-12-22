d7f975f6978ed9533550fe86661738950583c417
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-12-22 21:49:32 +0800

    created a startrecording, transcribe and stoprecording function that will be called on the event listener

--- Speech to text/textToSpeech.js
+++ Speech to text/textToSpeech.js
@@ -1,7 +1,35 @@
 let startBtn = document.getElementById('start-btn');
 let resultText = document.getElementById('resultDisplay');
 
+let isRecording = false
+let speechObj = null
+
 const speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
 const recognition = new speechRecognition()
 
-console.log(recognition)
+if(!recognition){
+    startBtn.textContent = "Speech Recognition not enabled"
+    startBtn.disabled = true
+}
+
+startBtn.addEventListener('click', (event)=>{
+    isRecording = !isRecording
+    isRecording ? startRecording() : stopRecording()
+})
+
+function startRecording(){
+    startBtn.textContent = "Recording..."
+    speechObj = new speechRecognition()
+    speechObj.start()
+    speechObj.onresult = transcribe
+}
+
+function transcribe(event){
+    console.log(event)
+}
+
+function stopRecording(){
+    speechObj.stop()
+    speechObj = null
+    startBtn.textContent = "Start"
+}
