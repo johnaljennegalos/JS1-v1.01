3458e42d1b5aa0f3b4c42a1a95d80a74f7119a58
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-12-22 21:27:38 +0800

    test the speech connection

--- Speech to text/textToSpeech.js
+++ Speech to text/textToSpeech.js
@@ -1,5 +1,7 @@
-const speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+let startBtn = document.getElementById('start-btn');
+let resultText = document.getElementById('resultDisplay');
 
+const speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
 const recognition = new speechRecognition()
 
 console.log(recognition)
