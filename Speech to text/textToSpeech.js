7834fd5078e8757c40fc2507b82c4e910ce8497a
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-12-22 22:05:48 +0800

    extract the recorded words from the transcript

--- Speech to text/textToSpeech.js
+++ Speech to text/textToSpeech.js
@@ -1,31 +1,44 @@
 let startBtn = document.getElementById('start-btn');
+let stopBtn =  document.getElementById('stop-btn');
 let resultText = document.getElementById('resultDisplay');
 
 let isRecording = false
 let speechObj = null
 
 const speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
-const recognition = new speechRecognition()
 
-if(!recognition){
+if(!speechRecognition){
     startBtn.textContent = "Speech Recognition not enabled"
     startBtn.disabled = true
 }
 
 startBtn.addEventListener('click', (event)=>{
     isRecording = !isRecording
-    isRecording ? startRecording() : stopRecording()
+    startRecording()
+})
+
+stopBtn.addEventListener('click', (event)=>{
+    isRecording = false
+    stopRecording()
 })
 
 function startRecording(){
     startBtn.textContent = "Recording..."
     speechObj = new speechRecognition()
-    speechObj.start()
     speechObj.onresult = transcribe
+    speechObj.start()
 }
 
 function transcribe(event){
     console.log(event)
+
+    let transcript = ""
+
+    for(let i = event.resultIndex; i < event.results.length; i++){
+        transcript += event.results[i][0].transcript
+    }
+
+
 }
 
 function stopRecording(){
