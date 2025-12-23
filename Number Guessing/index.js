620a2682cb3f05d266f4b6fabb63b44212198cb5
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-12-23 22:04:42 +0800

    created a random generator function

--- Number Guessing/index.js
+++ Number Guessing/index.js
@@ -3,3 +3,7 @@ let guessBtn = document.getElementById('guess-btn')
 let randomNum = document.getElementById('random-num')
 let resultEl = document.getElementById('result')
 
+function generateRandomNumber(){
+    randomNum = Math.floor(Math.random() * 100 + 1)
+    return  randomNum;
+}
