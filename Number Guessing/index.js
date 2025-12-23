12d755b3984fd338b758759393afd9768768709f
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-12-23 22:24:05 +0800

    created a reset function

--- Number Guessing/index.js
+++ Number Guessing/index.js
@@ -2,11 +2,32 @@ let inputEl = document.getElementById('input')
 let guessBtn = document.getElementById('guess-btn')
 let randomNum = document.getElementById('random-num')
 let resultEl = document.getElementById('result')
+let resetBtn = document.getElementById('reset-btn')
+let lifeEl =  document.getElementById('life')
 
 function generateRandomNumber(){
-    randomNum = Math.floor(Math.random() * 100 + 1)
+    let random = Math.floor(Math.random() * 100 + 1)
     console.log(randomNum)
-    return  randomNum;
+
+    randomNum.textContent = random
+
+    if(Number(inputEl.value) < random){
+        resultEl.textContent = 'Too low'
+    } else if(Number(inputEl.value) > random){
+        resultEl.textContent = 'Too high'
+    }
+
+    else {
+        resultEl.textContent = 'You Guess it!'
+    }
 }
 
-generateRandomNumber()
+function reset(){
+    randomNum.textContent = '0'
+    inputEl.value = ''
+    resultEl.textContent = ''
+}
+
+guessBtn.addEventListener('click', generateRandomNumber)
+
+resetBtn.addEventListener('click', reset)
