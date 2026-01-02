44798880f0b1bacdfeab6355359c2fa38e1b5226
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-01-02 20:47:28 +0800

    get element ID in js

---
+++ Pomodoro Timer/index.js
@@ -0,0 +1,7 @@
+let focusBtn = document.getElementById('focus-btn');
+let shortBreakBtn = document.getElementById('shortBreak-btn');
+let longBreakBtn = document.getElementById('longBreak-btn');
+let timeEl =  document.getElementById('time');
+let startBtn = document.getElementById('start-btn');
+let pauseBtn = document.getElementById('pause-btn');
+let resetBtn = document.getElementById('reset-btn');
