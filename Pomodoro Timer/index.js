8ec7d2b840100f1578d6d43e2a868366c6578eea
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-01-02 21:06:17 +0800

    created a timer function for focus

--- Pomodoro Timer/index.js
+++ Pomodoro Timer/index.js
@@ -5,3 +5,22 @@ let timeEl =  document.getElementById('time');
 let startBtn = document.getElementById('start-btn');
 let pauseBtn = document.getElementById('pause-btn');
 let resetBtn = document.getElementById('reset-btn');
+
+
+let timeLeft = 1500
+let minutes
+let seconds
+let interval
+
+
+function timer(){
+    timeLeft = timeLeft - 1
+    minutes = Math.floor(timeLeft / 60)
+    seconds = timeLeft % 60
+    timeEl.textContent = minutes + ':' + seconds
+}
+
+
+startBtn.addEventListener('click', function(){
+    interval = setInterval(timer, 1000)
+})
