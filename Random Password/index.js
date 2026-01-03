8a093f336a9c2fc7bede67e7bff615fdbdd8fff3
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-01-03 21:39:54 +0800

    use a different approach using the window.crypto.getRandomValues

--- Random Password/index.js
+++ Random Password/index.js
@@ -4,16 +4,26 @@ let resultEl =  document.getElementById('result');
 function generateRandomPassword(){
     let bucket = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-='
 
-    let length = 12
+    // let length = 12
     let password = '';
+    //
+    // for(let i = 0; i < length; i++) {
+    //     let char = Math.floor(Math.random() * bucket.length);
+    //     password += bucket[char];
+    // }
 
-    for(let i = 0; i < length; i++) {
-        let char = Math.floor(Math.random() * bucket.length);
-        password += bucket[char];
+    const random = new Uint32Array(10)
+    window.crypto.getRandomValues(random)
+
+    for(let i = 0; i < 10; i++){
+        password += bucket[random[i] % bucket.length]
     }
+
 
     resultEl.style.color = 'white'
     resultEl.innerText = password;
 }
 
 randomBtn.addEventListener('click', generateRandomPassword);
+
+
