3c389f4119f106a4f65a8a2b5b14ac2e8b4eb126
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-01-04 22:37:07 +0800

    add an event listener for the add button

--- Expense Tracker/index.js
+++ Expense Tracker/index.js
@@ -1,4 +1,4 @@
-let expense = document.getElementById('expense');
+let expenseName = document.getElementById('expense-name');
 let amount = document.getElementById('amount');
 let category = document.getElementById('category');
 let food = document.getElementById('food');
@@ -6,3 +6,33 @@ let transpo =  document.getElementById('transpo');
 let electricity = document.getElementById('electricity');
 let dateEl = document.getElementById('date');
 let addBtn = document.getElementById('add-btn');
+let formEl = document.getElementById('form');
+let expenseTable = document.getElementById('expense-list');
+
+let expenseList = []
+
+addBtn.addEventListener('click', (e)=>{
+    e.preventDefault();
+
+    const expense_name = expenseName.value;
+    const expense_amount = Number(amount.value);
+    const categories = category.value;
+    const date_element = dateEl.value;
+
+    const newExpense = {
+        id: Date.now(),
+        name: expense_name,
+        amount: expense_amount,
+        category: categories,
+        date: date_element
+    }
+
+    expenseList.push(newExpense);
+
+    console.log(expenseList);
+
+})
+
+
+
+
