1f2478088ae0e171f5f73d8cf219473b307a4996
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-01-06 20:51:44 +0800

    add button event listener prototype

--- Local Storage CRUD App/index.js
+++ Local Storage CRUD App/index.js
@@ -5,3 +5,25 @@ let emailEl = document.getElementById('email');
 let addressEl = document.getElementById('address');
 let addBtn = document.getElementById('add');
 let dataListEl = document.getElementById('data-list');
+
+
+let data = []
+
+addBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const dataName = nameEl.value;
+    const dataAge = Number(ageEl.value);
+    const dataEmail = emailEl.value;
+    const dataAddress = addressEl.value;
+
+    const newData = {
+        name: dataName,
+        age: dataAge,
+        email: dataEmail,
+        address: dataAddress
+    }
+
+    data.push(newData);
+
+})
