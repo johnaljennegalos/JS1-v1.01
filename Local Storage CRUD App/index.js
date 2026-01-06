cf9d04569aee763b1fcb0be9d5a5562d0cef1d93
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-01-06 20:48:01 +0800

    get element ID's

---
+++ Local Storage CRUD App/index.js
@@ -0,0 +1,7 @@
+let formEl = document.getElementById('form');
+let nameEl = document.getElementById('name');
+let ageEl = document.getElementById('age');
+let emailEl = document.getElementById('email');
+let addressEl = document.getElementById('address');
+let addBtn = document.getElementById('add');
+let dataListEl = document.getElementById('data-list');
