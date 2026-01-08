4e103496903c9a8315621d6502857e08ddddda49
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-01-08 21:40:53 +0800

    created an updateForm and validateInput function to update the UI and validate the input

--- Multi-step Form/index.js
+++ Multi-step Form/index.js
@@ -1,7 +1,36 @@
 let currentStep = 0
 let topStep = document.querySelectorAll('#top-content')
 let pageContent = document.querySelectorAll('#step-content')
+let nextBtn = document.querySelector('#next-btn')
+let previousBtn = document.querySelector('#previous-btn')
 
 function updateForm(){
+    for (let i = 0; i < pageContent.length; i++){
+        if(i === currentStep){
+            pageContent.classList.remove('hidden')
+        } else {
+            pageContent.classList.add('hidden')
+        }
+    }
 
+    nextBtn.addEventListener('click', () => {
+        if(validateInput()){
+            currentStep++
+        }
+
+        updateForm()
+    })
+
+}
+
+function validateInput(){
+    let name = document.getElementById('firstname').value
+    let lastname = document.getElementById('lastname').value
+    let dateOfBirth = document.getElementById('dateOfBirth').value
+
+    if(name === ''  || lastname === '' || dateOfBirth === ''){
+        return false
+    } else {
+        return true
+    }
 }
