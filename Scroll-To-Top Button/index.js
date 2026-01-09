b6af05f2c4f48bde01f226dc3fb206b4eade880b
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-01-09 19:13:56 +0800

    event listener on window scroll

--- Scroll-To-Top Button/index.js
+++ Scroll-To-Top Button/index.js
@@ -1,1 +1,11 @@
 let topBtn = document.getElementById('top-btn');
+
+window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY
+
+    if(scrollY > 20) {
+        topBtn.classList.remove('hidden')
+    } else {
+        topBtn.classList.add('hidden')
+    }
+})
