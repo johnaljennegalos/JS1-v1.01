ce6b79608fa8711a34cff136e0149299db4384cd
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-01-10 21:53:02 +0800

    fix the accordion to only show the active element and close the inactive

--- FAQ Accordion/index.js
+++ FAQ Accordion/index.js
@@ -2,6 +2,12 @@ let faqItem = document.querySelectorAll('.faq-item')
 
 faqItem.forEach(item => {
     item.addEventListener('click', (event) => {
-        document.querySelector('.item-answer').classList.toggle('hidden')
+        faqItem.forEach(otherItem => {
+            if(otherItem !== item){
+                otherItem.querySelector('.item-answer').classList.add('hidden')
+            }
+        })
+
+        item.querySelector('.item-answer').classList.toggle('hidden')
     })
 })
