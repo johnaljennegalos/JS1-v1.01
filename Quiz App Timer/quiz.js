00f8806dd94d2d33f020e20dc5f293568bc49652
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-01-16 22:04:09 +0800

    initialize the js

---
+++ Quiz App Timer/quiz.js
@@ -0,0 +1,26 @@
+const questions = [
+    {
+        question: "What does HTML stand for?",
+        options: ["Hyper Text Pre Processor", "Hyper Text Markup Language", "Hyper Tixt Marksteve Lala"],
+        correct: 1
+    },
+
+    {
+        question: "What does CSS stand for?",
+        options: ["Cascading Style Sheet", "CasCasSon", "Cascading Style Sheesh"],
+        correct: 1
+    },
+    {
+        question: "Which HTML tag is used to define an internal style sheet?",
+        options: ["<script>", "<css>", "<style>", "<link>"],
+        correct: 2
+    }
+]
+
+let startBtn = document.getElementById("startbtn");
+let quizScreen = document.getElementById("quiz-screen");
+let questionText = document.getElementById("question-text");
+let optionContainer = document.getElementById("option-container");
+let resultScreen = document.getElementById("result-screen");
+let finalScore = document.getElementById("final-score");
+let restartBtn = document.getElementById("restart-btn");
