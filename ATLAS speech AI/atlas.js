6141a8c0d31e7285aacd61e662c7563c213a1494
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-01-17 22:53:40 +0800

    setup the speech recognition and synthesis

--- ATLAS speech AI/atlas.js
+++ ATLAS speech AI/atlas.js
@@ -2,3 +2,37 @@ const speakBtn =  document.getElementById('speak');
 const statusEl = document.getElementById('status');
 const atlasImg = document.getElementById('atlas-img');
 
+const speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new speechRecognition();
+
+speakBtn.addEventListener('click', () => {
+    statusEl.textContent = "ATLAS Listening...";
+
+    recognition.start()
+});
+
+
+recognition.onstart = (event) => {
+    console.log(event);
+}
+
+recognition.onresult = (event) => {
+    let current = event.resultIndex
+
+    const transcript = event.results[current][0].transcript
+
+    console.log(transcript)
+
+    speakBack(transcript)
+}
+
+function speakBack(message){
+    const utterance = new SpeechSynthesisUtterance(message)
+
+    window.speechSynthesis.speak(utterance);
+}
+
+function takeCommand(){
+
+}
