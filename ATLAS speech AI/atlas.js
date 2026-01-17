42498006dfb25d635cf79bf4deda3e32b0761995
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-01-17 23:08:17 +0800

    add a fallback on the speaking condition and loop on speech

--- ATLAS speech AI/atlas.js
+++ ATLAS speech AI/atlas.js
@@ -24,15 +24,40 @@ recognition.onresult = (event) => {
 
     console.log(transcript)
 
-    speakBack(transcript)
+    takeCommand(transcript)
+
 }
 
 function speakBack(message){
     const utterance = new SpeechSynthesisUtterance(message)
 
+    utterance.rate = 1;
+    utterance.pitch = 1;
+    utterance.volume = 1;
+
+    atlasImg.classList.add('animate');
+
+    statusEl.textContent = "ATLAS speaking...";
+
+    utterance.onend = () => {
+        atlasImg.classList.remove('animate');
+        statusEl.textContent = "ATLAS listening...";
+
+        recognition.start()
+    };
+
     window.speechSynthesis.speak(utterance);
 }
 
-function takeCommand(){
+function takeCommand(message){
+    message = message.toLowerCase()
+
+    if(message.includes('hello') || message.includes('good')){
+        speakBack('Hi John, How can I help you Sir?')
+    } else if (message.includes('john') ||  message.includes('enciso')) {
+        speakBack('He is a handsome person')
+    } else {
+        speakBack('Sorry I didnt catch that')
+    }
 
 }
