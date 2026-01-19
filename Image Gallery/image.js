775761d2f72172b50b496c12d5d00148d51b0830
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-01-19 22:25:43 +0800

    initialize the js

---
+++ Image Gallery/image.js
@@ -0,0 +1,4 @@
+const mainImg = document.getElementById('main-image')
+const scrollContainer = document.getElementById('scroll-contain')
+const leftBtn = document.getElementById('left-btn')
+const rightBtn = document.getElementById('right-btn')
