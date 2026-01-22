d44000b8f2299ff5f0b0f2a091ddf009ef0efc9d
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-01-22 22:03:11 +0800

    created an event listener to listen and use the function to copy text

--- Copy To Clipboard Tool/index.js
+++ Copy To Clipboard Tool/index.js
@@ -1,3 +1,17 @@
 let copyInput = document.getElementById('copy-input');
 let copyBtn = document.getElementById('copy-button');
 let resultSpan = document.getElementById('result-span');
+
+copyBtn.addEventListener('click', (event) => {
+    copyText()
+})
+
+function copyText() {
+    copyInput.select()
+
+    navigator.clipboard.writeText(copyInput.value)
+
+    console.log('Copied!')
+
+    resultSpan.textContent = copyInput.value
+}
