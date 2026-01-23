62d41f4a44b336ba6ce4cd5a9bc567779bfa6efe
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-01-23 21:43:25 +0800

    create a event listener on fileUpload with function on onload

--- Image Upload and Preview/preview.js
+++ Image Upload and Preview/preview.js
@@ -1,2 +1,11 @@
 let fileUpload = document.getElementById('file-upload');
 let imagePreview =  document.getElementById('image-preview');
+
+fileUpload.addEventListener('change', (event) => {
+    let file = new FileReader()
+
+    file.onload = function (event) {
+        imagePreview.src = file.result
+        imagePreview.classList.remove('hidden')
+    }
+})
