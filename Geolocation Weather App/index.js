1ccd1990ff0ff6365078a28c8f07027ac153b506
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-01-24 21:19:32 +0800

    added an event listener for button to check for location

--- Geolocation Weather App/index.js
+++ Geolocation Weather App/index.js
@@ -3,3 +3,11 @@ let dataBox = document.querySelectorAll('.data-box')
 let latitudeEl = document.getElementById('latitude');
 let longitudeEl = document.getElementById('longitude');
 let mapLink = document.getElementById('map-link');
+
+findMeButton.addEventListener('click', () => {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition()
+    } else {
+        console.log("Browser not supported")
+    }
+})
