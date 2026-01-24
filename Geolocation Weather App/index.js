12d477609724a723711b557c10b1c2d66a2eba7a
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-01-24 21:11:16 +0800

    initialize js ID

---
+++ Geolocation Weather App/index.js
@@ -0,0 +1,5 @@
+let findMeButton = document.getElementById('find-me-button');
+let dataBox = document.querySelectorAll('.data-box')
+let latitudeEl = document.getElementById('latitude');
+let longitudeEl = document.getElementById('longitude');
+let mapLink = document.getElementById('map-link');
