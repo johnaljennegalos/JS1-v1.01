a337692d6e6e018f689ecca7ebd83d6f21c0272e
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-01-25 22:00:20 +0800

    chart prototyping

--- Weather Forecast with Charts/chart.js
+++ Weather Forecast with Charts/chart.js
@@ -1,1 +1,13 @@
 let chartCanvas =  document.getElementById('chart-canvas');
+
+const myChart = new Chart(chartCanvas, {
+    type: 'line',
+    data: {
+        labels: ['1 PM', '2 PM', '3 PM', '4 PM', '5 PM'],
+        datasets: [{
+            labels: 'Temperature',
+            data: [20, 22, 19, 24, 25],
+            borderWidth: 1
+        }]
+    }
+})
