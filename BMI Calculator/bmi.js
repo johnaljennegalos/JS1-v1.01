27c7abb4c7a96d5582897bf6f05a57da5f98e29b
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-01-27 22:46:44 +0800

    comment out unused variables

--- BMI Calculator/bmi.js
+++ BMI Calculator/bmi.js
@@ -1,11 +1,11 @@
 let formEl =  document.getElementById('form');
 let standardContainer = document.getElementById('standard-container');
-let standardBtn1 = document.getElementById('standard1-btn')
-let metricBtn1 = document.getElementById('metric1-btn')
+// let standardBtn1 = document.getElementById('standard1-btn')
+// let metricBtn1 = document.getElementById('metric1-btn')
 
 let metricContainer = document.getElementById('metric-container');
 let standardBtn2 = document.getElementById('standard2-btn')
-let metricBtn2 = document.getElementById('metric2-btn')
+// let metricBtn2 = document.getElementById('metric2-btn')
 
 let centimeterEl = document.getElementById('centimeters');
 let weightEl = document.getElementById('weight');
